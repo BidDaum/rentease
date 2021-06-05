ae93d88762e1bf3273f47e420e9f9be72c61134d
William <allinwilliam@gmail.com>
2021-06-05 14:33:59 +0200

    merged

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :applies, only: [ :new, :create, :index, :show ] do
+  resources :applies, only: [ :new, :create, :index, :show, destroy ] do
     resources :rentfiles, only: [:new, :create]
   end
   devise_for :users
